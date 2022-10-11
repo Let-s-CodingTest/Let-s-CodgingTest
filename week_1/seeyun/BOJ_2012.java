394d87d7ea69517a7d01f3348c28c3515a7ef69e
seanee3670 <cseok3670@gmail.com>
2022-10-12 00:50:36 +0900

    week1_seeyun [*]

---
+++ week_1/seeyun/BOJ_2012.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class BOJ_2012 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] grades = new int[N + 1];
+
+        for (int i = 1; i < grades.length; i++) {
+            grades[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(grades);
+
+        long sum = 0;
+
+        for (int i = 1; i < grades.length; i++) {
+            int degree = grades[i] - i;
+
+            if (degree < 0) {
+                degree *= (-1);
+            }
+            sum += degree;
+        }
+
+        System.out.println(sum);
+    }
+}
