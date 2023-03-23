5c5d90fc797c3c5e5c4833576d933b27206f27f8
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2023-03-23 23:38:11 +0900

    week20_minji [PG_132265]

---
+++ week_21/minji/PG_132265.py
@@ -0,0 +1,19 @@
+from collections import Counter
+
+def solution(topping):
+    answer = 0
+    A_dict = {}
+    B_dict = Counter(topping)
+
+    if len(topping) == 1: return answer
+
+    for i in range(len(topping)):
+        if B_dict[topping[i]] > 1:
+            B_dict[topping[i]] -= 1
+        elif B_dict[topping[i]] == 1:
+            del B_dict[topping[i]]
+        A_dict[topping[i]] = A_dict.get(topping[i], 0) + 1
+        if len(B_dict) == len(A_dict):
+            answer += 1
+
+    return answer
