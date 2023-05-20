291f8f5f2fce9c47021df374848106b0025ae7f2
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2023-05-20 18:17:27 +0900

    week25_minji [PG_42579, PG_68936]

---
+++ week_25/PG_42579/minji.py
@@ -0,0 +1,19 @@
+from collections import defaultdict
+
+
+def solution(genres, plays):
+    answer = []
+    genre_plays = defaultdict(int)  # 장르별 재생횟수
+    genre = defaultdict(list)  # 장르별 [재생횟수, 고유번호]
+
+    for idx in range(len(genres)):
+        genre[genres[idx]].append([plays[idx], idx])
+        genre_plays[genres[idx]] += plays[idx]
+
+    genre_plays = sorted(genre_plays.items(), key=lambda x: -x[1])  # 재생횟수 많은순
+
+    for key, value in genre_plays:
+        genre[key].sort(key=lambda x: -x[0])  # 재생횟수 내림차순 정렬
+        for play, idx in genre[key][:2]:  # 장르별로 두곡
+            answer.append(idx)
+    return answer
